a5be73b46703454c4d17d885a4194b521513d767
yamagata-honoka <h.yamagata1204@gmail.com>
2026-06-05 10:37:09 +0000

    第5回: Python & FastAPI⼊⾨の 実習コードを追加

--- session05/exercise/basics.py
+++ session05/exercise/basics.py
@@ -16,19 +16,19 @@
 """
 
 # ヒント: TODOリストを作成する
-#   todos = [
-#       {"id": 1, "title": "課題を出す", "done": False},
-#       {"id": 2, "title": "買い物する", "done": True},
-#       {"id": 3, "title": "自分のTODO", "done": False},
-#   ]
+todos = [
+  {"id": 1, "title": "課題を出す", "done": False},
+  {"id": 2, "title": "買い物する", "done": True},
+  {"id": 3, "title": "自分のTODO", "done": False},
+]
 
 
 # ヒント: TODOを1件ずつ表示する関数を作成する
-#   def show_todos(todo_list):
-#       for todo in todo_list:
-#           status = "[x]" if todo["done"] else "[ ]"
-#           print(f'{status} {todo["id"]}: {todo["title"]}')
+def show_todos(todo_list):
+  for todo in todo_list:
+    status = "[x]" if todo["done"] else "[ ]"
+    print(f'{status} {todo["id"]}: {todo["title"]}')
 
 
 # ヒント: 関数を呼び出して動作を確認する
-#   show_todos(todos)
+show_todos(todos)
--- session05/exercise/main.py
+++ session05/exercise/main.py
@@ -28,25 +28,30 @@ def root():
 # エンドポイント2: 名前付き挨拶
 # -----------------------------------------------
 # ヒント:
-#   @app.get("/hello/{name}")
-#   def hello(name: str):
-#       return {"message": f"こんにちは、{name}さん！"}
+@app.get("/hello/{name}")
+def hello(name: str):
+    return {"message": f"こんにちは、{name}さん！"}
 
 
 # -----------------------------------------------
 # エンドポイント3: TODOリスト取得
 # -----------------------------------------------
 # ヒント: まずTODOデータを作成
-#   todos = [
-#       {"id": 1, "title": "買い物に行く", "done": False},
-#       {"id": 2, "title": "レポートを書く", "done": True},
-#       {"id": 3, "title": "部屋を掃除する", "done": False},
-#   ]
+todos = [
+       {"id": 1, "title": "買い物に行く", "done": False},
+       {"id": 2, "title": "レポートを書く", "done": True},
+       {"id": 3, "title": "部屋を掃除する", "done": False},
+   ]
 #
 # ヒント: エンドポイントを作成
-#   @app.get("/todos")
-#   def get_todos():
-#       return todos
+@app.get("/todos")
+def get_todos():
+    return todos
+
+@app.get("/todo/{id}")
+def get_todo(id):
+    index = int(id) - 1
+    return todos[index]
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
